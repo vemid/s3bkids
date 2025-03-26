60a5fdce427ca3178a233864581575d4302645fa
vemid <admin@bebakids.com>
2025-03-26 15:52:06 +0100

    tes

--- product-api/index.js
+++ product-api/index.js
@@ -30,8 +30,8 @@ app.use('/api/auth', authRoutes);
 
 // Konfiguracija Minio klijenta
 const minioClient = new Minio.Client({
-    endPoint: process.env.MINIO_ENDPOINT || 'localhost',
-    port: parseInt(process.env.MINIO_PORT || '9000'),
+    endPoint: process.env.MINIO_ENDPOINT || 'https://s3bkids.bebakids.com',
+    port: parseInt(process.env.MINIO_PORT || '443'),
     useSSL: process.env.MINIO_USE_SSL === 'true',
     accessKey: process.env.MINIO_ACCESS_KEY,
     secretKey: process.env.MINIO_SECRET_KEY
